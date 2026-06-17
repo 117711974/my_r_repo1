16ea787fb1ea68ab7a1401c84a5796ddc58859c0
John S <u1171240@utah.edu>
2026-06-17 14:03:05 -0600

    Added CO2 data and plot

--- lmExample.R
+++ lmExample.R
@@ -5,3 +5,8 @@ summary(fit)
 
 plot(x,y)
 abline(fit, col=2)
+setwd("C:\\School\\2026\\Summer 2026\\GEOG5680\\Data")
+co2<-read.csv("co2_mm_mlo.csv")
+plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
+     xlab="Time", ylab="ppm")
+lines(co2$decdate, co2$trend, col=2, lwd=2)
